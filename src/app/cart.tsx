394c258b0bc78052fdef4f739d82fa6b96bfb410
Edgar Rafael Conrado Hernandez <edgar.conrado@oreillyauto.com>
2024-11-20 12:14:07 -0600

    Addign Total en the CartList component

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,22 +1,42 @@
+import Button from '@/components/Button';
 import CartListItem from '@/components/CartListItem';
 import { useCart } from '@/providers/CartProviders';
-import { StatusBar } from 'expo-status-bar'
-import { FlatList, Platform, Text, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar';
+import { FlatList, Platform, StyleSheet, Text, View } from 'react-native';
 
 const CartScreen = () => {
 
-  const { items } = useCart();
+  const { items, total } = useCart();
 
   return (
-    <View>
+    <View style={styles.container}>
       <FlatList
         data={items}
         renderItem={({ item }) => <CartListItem cartItem={item} />}
+        contentContainerStyle= {styles.flatListContainer}
       />
+
+      <Text style={ styles.total}>Total: ${total}</Text>
+      <Button text='Checkout' />
 
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  flatListContainer: {
+    gap: 10,
+  },
+  total: {
+    marginTop: 20, 
+    fontSize: 20,
+    fontWeight: '500',
+  }
+
+});
 
 export default CartScreen;
