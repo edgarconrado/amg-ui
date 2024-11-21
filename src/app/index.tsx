dbabc7c18909b3dd527f0ffda1d570ee9f770f4d
Edgar Rafael Conrado Hernandez <edgar.conrado@oreillyauto.com>
2024-11-21 15:09:25 -0600

    Creating form to signin and signup (ui)

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import Button from '../components/Button';
 import { Link } from 'expo-router';
+import React from 'react';
+import { View } from 'react-native';
+import Button from '../components/Button';
 
 const index = () => {
   return (
@@ -11,6 +11,9 @@ const index = () => {
       </Link>
       <Link href={'/(admin)'} asChild>
         <Button text="Admin" />
+      </Link>
+      <Link href={'/sign-in'} asChild>
+        <Button text="Sign In" />
       </Link>
     </View>
   );
